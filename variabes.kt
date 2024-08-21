4b12a9a2cc25450fa29521c8e5ffbef130171bd0
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-21 13:34:17 +0530

    variables explained

---
+++ variabes.kt
@@ -0,0 +1,16 @@
+import com.sun.jdi.IntegerType
+
+fun main() {
+  var a = readLine()!!.toInt()
+  val b = readLine()!!.toInt() // val is const type which value cant be changed
+
+//    var c:Int
+//    c = a + b
+   // println("******** " + c + " *******")
+    // take input and print which is greater
+//    val a = readLine()
+//    val b = readLine().
+    print("Is a is greater than b: ")
+    println(a>b)
+}
+
