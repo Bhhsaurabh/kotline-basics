09f4179d43472419fcd1cc9402865dfca64730c6
Saurabh Kumar <120705065+Bhhsaurabh@users.noreply.github.com>
2024-08-26 13:53:58 +0530

    inv|when|shift|for|while|do while explained

---
+++ Myclass.kt
@@ -0,0 +1,46 @@
+class Myclass {
+    companion object{
+      @JvmStatic
+      fun main(args:Array<String>){
+//          print("hello saurabh")
+          var a = 5
+          // right shift shr , left shift shl
+//          println(a shl  2)
+          var x = 20
+          var y = 30
+          // inv operation
+          val newNum = x.inv()
+
+          //elvis operator
+          val str:String? = null
+//          println(str)
+//
+          // switches
+          val choice = 2
+          val result = when(choice){
+              1->"hello"
+              2->"hii"
+              3->"why"
+              4->"where"
+              else -> "not found"
+          }
+//          println(result)
+          for(i in 1..5){
+//              println(i)
+          }
+          for(i in 5 downTo 2){
+//              println(i)
+          }
+          for(i in 1..10 step 2){
+//              println(i)
+          }
+          // while loop
+          var ar:Int = 2
+          //
+          while(ar<10){
+             // println(ar)
+              ar++
+          }
+      }
+    }
+}
